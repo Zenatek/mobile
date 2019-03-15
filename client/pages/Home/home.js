64611be6fce608781b8ad3be9d7af05baf8d638c
Rootx86 <filippo.lapide@gmail.com>
2019-03-15 16:52:40 +0100

    fix fullscreen background

--- client/pages/Home/home.js
+++ client/pages/Home/home.js
@@ -1,5 +1,8 @@
 Template.Home.helpers({
     redirectDashboard : function () {
         FlowRouter.go("/dashboard")
-    }
+    },
+    changeBackground : function () {
+            $('body').css('background-image','url(/login.jpg)');
+        }
 });
